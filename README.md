7b7ac0d9a6c0981aa6c1c6ab9a194e98014ed0ad
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-08-07 23:43:34 -0300

    Adicionando requisitos no sistema e comentando novos comportamentos no serviço.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Repositório para o curso de Testes automatizados com Spring Framework.
 Requisitos:
 CARRO: 2 reais a cada 30 minutos. Após 4 horas, cada hora adicional custa 5 reais. Tolerância de 15 minutos.
 MOTO: 1 real a cada 30 minutos. Após 4 horas, cada hora adicional custa 3 reais. Tolerância de 15 minutos.
+
+* É preciso salvar no banco de dados quando um veículo entra e quando um veículo sai;
+* É preciso consultar no banco de dados a última vez que o veículo registrou saída no estacionamento para cobrança. Pesquisa deve ser realizada pela placa do veículo.
--- src/main/java/br/com/oobj/estacionamento/servico/impl/EstacionamentoServicoImpl.java
+++ src/main/java/br/com/oobj/estacionamento/servico/impl/EstacionamentoServicoImpl.java
@@ -9,13 +9,20 @@ public class EstacionamentoServicoImpl implements EstacionamentoServico {
 
 	public void registrarEntrada(Veiculo veiculo) {
 		veiculo.setDataHoraEntrada(new Date());
+		
+		// salvar registro do veículo no banco (novo registro)
 	}
 
 	public void registrarSaida(Veiculo veiculo) {
+		// buscar última entrada do veículo no estacionamento pela placa
 		veiculo.setDataHoraSaida(new Date());
+		
+		// atualizar registro do veiculo no banco
 	}
 
 	public Double calcularValor(Veiculo veiculo) {
+		// buscar ultimo registro do veiculo no banco
+		
 		return veiculo.getTipoVeiculo().getCalculadora().calcular(veiculo);
 	}
 
