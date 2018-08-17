d158cfd2d9449270654cfab4814df3017458c719
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-08-17 15:01:08 -0300

    Ajustando testes

--- src/main/java/br/com/oobj/estacionamento/servico/CalculadoraEstacionamento.java
+++
@@ -1,9 +0,0 @@
-package br.com.oobj.estacionamento.servico;
-
-import br.com.oobj.estacionamento.modelo.Veiculo;
-
-public interface CalculadoraEstacionamento {
-	
-	Double calcular(Veiculo veiculo);
-
-}
